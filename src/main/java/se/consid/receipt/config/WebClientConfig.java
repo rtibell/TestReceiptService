03178ebe503740d57c9155513927f34043e8732d
rasmus.tibell <rasmus.tibell@gmail.com>
2024-04-19 15:17:55 +0200

    Change WebClient DNS resolver

--- src/main/java/se/consid/receipt/config/WebClientConfig.java
+++ src/main/java/se/consid/receipt/config/WebClientConfig.java
@@ -1,15 +1,26 @@
 package se.consid.receipt.config;
 
+import io.netty.resolver.DefaultAddressResolverGroup;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.resources.ConnectionProvider;
 
 @Configuration
 public class WebClientConfig {
     @LoadBalanced
     @Bean
     public WebClient.Builder webClientBuilder() {
+        HttpClient httpClient = HttpClient.create(ConnectionProvider.create("myConnectionProvider"))
+                .tcpConfiguration(client ->
+                        client.bootstrap(bootstrap ->
+                                //Way to modify tcp client connect settings.
+                                bootstrap.resolver(DefaultAddressResolverGroup.INSTANCE)));
         return WebClient.builder();
     }
+//    public WebClient.Builder webClientBuilder() {
+//        return WebClient.builder();
+//    }
 }
